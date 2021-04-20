3d283417db0e20505b6a0948d52b7ad660dd5194
Keri Gannon <keri.gannon@protonmail.com>
2021-04-19 21:24:17 -0500

    add todos

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,3 +3,12 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return <div className={styles.container}>Hello</div>;
 }
+
+// TODO: Add page for edit todo
+// TODO: Add page for delete todo
+// TODO: Add page for create todo
+// TODO: Research redirecting this page to todos
+// TODO: Research what to put in /api
+// TODO: Research where to put styled components
+// TODO: Research where to put types
+// TODO: Research where to put env variables
